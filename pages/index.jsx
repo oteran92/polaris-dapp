17282b881c3f86b22dc7b247187a069ec40e1f6b
Osmel Teran <osmelprieto92@gmail.com>
2023-05-24 15:04:34 +0200

    graphql query

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,29 @@
 import styles from "../styles/Home.module.css";
 import InstructionsComponent from "../components/InstructionsComponent";
+import { GraphQLClient } from "graphql";
+const client = new GraphQLClient("https://polaris-platform.saleor.cloud/graphql/");
+
+const query = `
+query {
+  products {
+    id
+    name
+    image
+    description
+  }
+}`;
+const response = await client.query(query);
+const products = response.data.products;
+<ul>
+  {products.map((product) => (
+    <li key={product.id}>
+      <h2>{product.name}</h2>
+      <img src={product.image} />
+      <p>{product.description}</p>
+    </li>
+  ))}
+</ul>
+
 
 export default function Home() {
   return (
